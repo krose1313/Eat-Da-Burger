569a06bea2defb2c6a3b9af1a755cee55c60d4a2
krose1313 <kathrynrose40@gmail.com>
2018-04-18 21:03:31 -0700

    updates

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,6 @@ const connection = mysql.createConnection({
   host: "fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "uzk8rmdadugbdjtp",
   password: "olqz8jiqdlba3ghg",
-  port: "3306",
   database: "hjp1s80ha0ae6qyi"
 });
 
