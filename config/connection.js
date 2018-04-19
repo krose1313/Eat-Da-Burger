d5b0b1e543f19baab46858bd74bd9e0852ac2ce2
krose1313 <kathrynrose40@gmail.com>
2018-04-18 21:06:07 -0700

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,4 @@
-const express = require("express");
 const mysql  = require('mysql');
-var app = express();
-app.use(express.logger());
 
 const connection = mysql.createConnection({
   host: "fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
